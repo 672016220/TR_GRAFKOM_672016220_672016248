2450828406044592a6b3c27d1a699a70344af81f
672016220 <67912720+672016220@users.noreply.github.com>
2020-07-25 04:22:47 +0700

    atap pojok kiri atas

--- TR_GRAFKOM_K_672016220_672016248.cpp
+++ TR_GRAFKOM_K_672016220_672016248.cpp
@@ -1305,8 +1305,36 @@ void atap(void){
     glColor3d(0.5,0.8,0.7);
     glVertex3d(-58,-48,26);
     glVertex3d(-66,-40,26);
-    glEnd();	
-
+    glEnd();
+	
+    glBegin(GL_POLYGON);//Pojok kiri atas,tengah atas
+    glColor3d(0.5,0.7,0.6);
+    glVertex3d(-54,32,30);
+    glVertex3d(-50,36,30);
+    glVertex3d(-50,44,30);
+    glVertex3d(-62,32,30);
+    glEnd();
+    glBegin(GL_TRIANGLES);//Pojok kiri,kiri
+    glColor3d(0.5,0.7,0.6);
+    glVertex3d(-70,32,26);
+    glVertex3d(-66,40,26);
+    glVertex3d(-62,32,30);
+    glEnd();
+    glBegin(GL_TRIANGLES);//Pojok kiri,kanan
+    glColor3d(0.5,0.7,0.6);
+    glVertex3d(-50,52,26);
+    glVertex3d(-58,48,26);
+    glVertex3d(-50,44,30);
+    glEnd();
+    glBegin(GL_POLYGON);//Pojok kiri,tengah bawah
+    glColor3d(0.5,0.7,0.6);
+    glVertex3d(-62,32,30);
+    glVertex3d(-50,44,30);
+    glColor3d(0.5,0.8,0.7);
+    glVertex3d(-58,48,26);
+    glVertex3d(-66,40,26);
+    glEnd();
+	
 }
 
 void display(void)
